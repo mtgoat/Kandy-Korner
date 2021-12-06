e60d3b371cdfd020f7dabdd806412481af0a3c5b
john gallagher <127highstreet@gmail.com>
2021-12-06 11:46:04 -0500

    update on the location listing

--- src/Components/locations/LocationCard.js
+++ src/Components/locations/LocationCard.js
@@ -7,6 +7,6 @@ export const LocationCard = ({location}) => (
         <h3 className="location__name">{location.name}</h3>
         <div className="location__add">{location.address}</div>
         <div className="location__sqF">{location.squareFootage}</div>
-        
+        {location.HandicapAccessiblity ? <div className="location__Hndy">This location is handicap accessible</div>: <div className="location__Hndy">This location is NOT handicap accessible</div> }
     </section>
 )
