1ff6ec7576798a4c8f04b730cc370bc189ff21fc
john gallagher <127highstreet@gmail.com>
2021-12-06 20:49:58 -0500

    products display

--- src/Components/products/ProductCard.js
+++ src/Components/products/ProductCard.js
@@ -2,10 +2,21 @@ import React from "react"
 import './Product.css'
 
 
-export const ProductCard = ({product}) => (
+export const ProductCard = ({product}) => {
+
+    const priceN = Number(product.price)
+    let PriceDis ={
+        style:"currency",
+        currency: "USD",
+    }
+
+    return (
+
     <section className="product">
         <h3 className="product__name">{product.name}</h3>
-        <div className="product__add">{product.productType.name}</div>
+        <div className="product__price">{priceN.toLocaleString("en-US", PriceDis)}</div>
+        <div className="product__typeName">{product.productType.name}</div>
        
     </section>
-)
+    )
+    }
