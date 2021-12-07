f34b6063ee5e876ae304d77f20c451b0127a1920
john gallagher <127highstreet@gmail.com>
2021-12-07 00:22:46 -0500

    error message on useNagigate

--- src/Components/employees/EmployeeCard.js
+++ src/Components/employees/EmployeeCard.js
@@ -4,7 +4,8 @@ import './Employee.css'
 
 export const EmployeeCard = ({employee}) => (
     <section className="employee">
-        <h3 className="employee__name">{employee.name}</h3>
+        <h3 className="employee__name">{employee.firstName} {employee.lastName}</h3>
+        {employee.manager? <div className="employee__mngr">Manager</div>: <span></span>}
         <div className="employee__addName">{employee.location.name}</div>
         <div className="employee__add">{employee.location.address}</div>
         
